3aa14c3c744d2483322a9ba8273b5a044ffd7679
Michael Humphrey <mhumphre@rackspace.com>
2019-07-22 17:37:50 -0500

    adds regional waf

--- services/rms-proxy/main.tf
+++ services/rms-proxy/main.tf
@@ -156,6 +156,47 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+  #Regional WAF
+  module "regional_waf" {
+  source    = "github.com/y0y0dyn3/xplat-terraform-modules//modules/regional-waf"
+  api_name  = "${var.service_name}"
+  stage     = "${var.stage}"
+  region    = "${var.region}"
+  
+  acl_association_resource_arn = "${aws_lb.main.arn}"
+
+  # Valid values are BLOCK or ALLOW
+  # The correct setting is almost always ALLOW.
+  # Default in variables.tf = ALLOW.
+  web_acl_default_action = "ALLOW"
+
+  # Valid values are BLOCK, ALLOW, COUNT.
+  # BLOCK will typically be the correct production value.
+  # Set to COUNT when introducing a new rule, until you 
+  # are certain that rule is behaving as intended.
+  # Default in variables.tf = COUNT.
+  ip_blacklist_default_action         = "COUNT" # currently an empty set.  Use the UI to add new IPs in an emergency.
+  rate_ip_throttle_default_action     = "COUNT"
+  xss_match_rule_default_action       = "COUNT"
+  byte_match_traversal_default_action = "COUNT"
+  byte_match_webroot_default_action   = "COUNT"
+  sql_injection_default_action        = "COUNT"
+
+  # Requests per 5 minutes.  Default in variables.tf = 5000.
+  rate_ip_throttle_limit = 2000
+
+
+  # Default Value is 0.  This is an all or nothing setting.
+  # All conditions, rules, WebACLS, and WAF assoctiations
+  # are governed by this value.
+  #
+  # When testing WAF chnages, USE --stage test with xphat, not 
+  # your personal stage.  This is due to low resource limits for 
+  # AWS WAFS.
+
+  enabled = "${var.stage == "dev" || var.stage == "prod" ? 1 : 0}"
+}
+
 # Cloudwatch
 resource "aws_cloudwatch_log_group" "rms-proxy" {
   name = "/ecs/${var.stage}-rms-proxy"
@@ -238,3 +279,6 @@ resource "aws_route53_record" "packages" {
     evaluate_target_health = true
   }
 }
+
+
+
